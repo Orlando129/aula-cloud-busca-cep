91ed7a032fc579b2b318b2613557a0df637d37a7
Orlando <orlandotelles129@gmail.com>
2025-09-16 22:55:57 +0000

    Ajeitando erro feito ao dar merge nas branchs

--- js/init.js
+++ js/init.js
@@ -1,8 +1,25 @@
 (function($){
   $(function(){
-
+    // Inicializar componentes do Materialize
     $('.sidenav').sidenav();
-    $('.tabs').tabs();
+    
+    // Inicializar tabs
+    $('.tabs').tabs({
+      onShow: function(content) {
+        // Carregar logs quando a aba de logs for exibida
+        if (content[0] && content[0].id === 'test3') {
+          setTimeout(carregarLogs, 100);
+        }
+      }
+    });
+    
+    // Carregar estados quando a página carregar
+    if (typeof pegarEstados === 'function') {
+      pegarEstados();
+    }
+    
+    // Esconder o loading inicial
+    $(".preloader-wrapper").hide();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
